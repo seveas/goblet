88224b67d94bf21c1ac61b54540716d06b8ccfc2
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2013-08-25 20:08:49 +0200

    Bugfix version 0.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name = "goblet",
-      version = "0.3",
+      version = "0.3.1",
       author = "Dennis Kaarsemaker",
       author_email = "dennis@kaarsemaker.net",
       url = "http://seveas.github.com/goblet",
